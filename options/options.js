3e765a335a2ce4fec93975c7847f5b4fd1c2281c
Crowdbotics <team@crowdbotics.com>
2021-08-11 17:21:27 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "kwame_gh_29724Identifier",
+  url: "https://kwame_gh_29724Identifier.botics.co",
+  api: "https://kwame_gh_29724Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
